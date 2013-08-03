ca5a09e90bfbe9be3e138d4032178357f9fcc48a
Matthieu Napoli <matthieu@mnapoli.fr>
2013-08-03 10:47:03 +0200

    PSR1/2 compatibility

--- src/DI/ZendFramework2/InjectedAbstractActionController.php
+++ src/DI/ZendFramework2/InjectedAbstractActionController.php
@@ -2,26 +2,31 @@
 namespace DI\ZendFramework2;
 
 use DI\Container;
-use DI\Annotation\Inject;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Stdlib\RequestInterface as Request;
 use Zend\Stdlib\ResponseInterface as Response;
 
 abstract class InjectedAbstractActionController extends AbstractActionController
 {
-	protected $container;
+    /**
+     * @var \DI\Container
+     */
+    protected $container;
 
-	public function __construct(Container $container = null)
-	{
-		$this->container = $container;
-	}
+    public function __construct(Container $container = null)
+    {
+        $this->container = $container;
+    }
 
-	public function dispatch(Request $request, Response $response = null)
-	{
-		if ($this->container == null)
-			$this->container = $this->serviceLocator->get('DI\\Container');
+    public function dispatch(Request $request, Response $response = null)
+    {
+        if ($this->container == null) {
+            $this->container = $this->serviceLocator->get('DI\\Container');
+        }
 
-		$this->container->injectOn($this);
-		return parent::dispatch($request, $response);
-	}
+        // Inject dependencies
+        $this->container->injectOn($this);
+
+        return parent::dispatch($request, $response);
+    }
 }
